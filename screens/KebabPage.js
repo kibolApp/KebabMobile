17007d7b7aac02b3ec61db67d7a0b240c84e8eb0
Tomasz Barczycki <115528520+TomaszBarczycki@users.noreply.github.com>
2025-01-10 16:07:35 +0100

    eslint-fixed

--- screens/KebabPage.js
+++ screens/KebabPage.js
@@ -6,7 +6,6 @@ import {
   TouchableOpacity,
   Linking,
   ScrollView,
-  StyleSheet,
 } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
